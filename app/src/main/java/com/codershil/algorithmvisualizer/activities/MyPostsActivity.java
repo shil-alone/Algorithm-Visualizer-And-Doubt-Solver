17fbb55343b1589cfd797bfa99eedfef0b007a3f
shil-alone <shilalone007@gmail.com>
2021-05-27 20:14:37 +0530

    added activity to see users posts

--- app/src/main/java/com/codershil/algorithmvisualizer/activities/MyPostsActivity.java
+++ app/src/main/java/com/codershil/algorithmvisualizer/activities/MyPostsActivity.java
@@ -1,14 +1,98 @@
 package com.codershil.algorithmvisualizer.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MyPostsActivity extends AppCompatActivity {
+import com.codershil.algorithmvisualizer.R;
+import com.codershil.algorithmvisualizer.adapters.PostAdapter;
+import com.codershil.algorithmvisualizer.models.Post;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+public class MyPostsActivity extends AppCompatActivity implements PostAdapter.OnItemClickListener {
+    RecyclerView postRV;
+    PostAdapter adapter;
+    SwipeRefreshLayout swipeRefreshLayout;
+    FirebaseFirestore database = FirebaseFirestore.getInstance();
+    CollectionReference postRef = database.collection("posts");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_posts);
+        initialize();
+        setUpRecyclerView();
+        addOnClicks();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    private void initialize() {
+        postRV = findViewById(R.id.myPostsRV);
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout1);
+    }
+
+    private void setUpRecyclerView() {
+        Query query = postRef.orderBy("createdAt", Query.Direction.DESCENDING).whereEqualTo("userUid", FirebaseAuth.getInstance().getCurrentUser().getUid());
+        FirestoreRecyclerOptions<Post> options = new FirestoreRecyclerOptions.Builder<Post>()
+                .setQuery(query, Post.class)
+                .build();
+        adapter = new PostAdapter(options);
+        postRV.setHasFixedSize(true);
+        postRV.setLayoutManager(new LinearLayoutManager(MyPostsActivity.this));
+        postRV.setAdapter(adapter);
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,
+                ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                adapter.deletePost(viewHolder.getAdapterPosition());
+            }
+        }).attachToRecyclerView(postRV);
+
+        adapter.onItemClickListener(this);
+    }
+
+
+    private void addOnClicks() {
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                adapter.notifyDataSetChanged();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+    }
+
+    @Override
+    public void onItemClick(DocumentSnapshot documentSnapshot, int position) {
+        Post post = documentSnapshot.toObject(Post.class);
+        Toast.makeText(this, post.getUserName(), Toast.LENGTH_SHORT).show();
     }
 }
