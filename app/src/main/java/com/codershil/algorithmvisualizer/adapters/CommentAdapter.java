61c27c596d0c689abcadf280094e3c1bdcb8c8a5
shil-alone <shilalone007@gmail.com>
2021-06-01 16:02:13 +0530

    added adapter class for showing comments

--- app/src/main/java/com/codershil/algorithmvisualizer/adapters/CommentAdapter.java
+++ app/src/main/java/com/codershil/algorithmvisualizer/adapters/CommentAdapter.java
@@ -1,4 +1,57 @@
 package com.codershil.algorithmvisualizer.adapters;
 
-public class CommentAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.view.inputmethod.InputConnectionCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.codershil.algorithmvisualizer.R;
+import com.codershil.algorithmvisualizer.models.Comment;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+public class CommentAdapter extends FirestoreRecyclerAdapter<Comment, CommentAdapter.CommentHolder> {
+
+    /**
+     * Create a new RecyclerView adapter that listens to a Firestore Query.  See {@link
+     * FirestoreRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public CommentAdapter(@NonNull FirestoreRecyclerOptions<Comment> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull CommentHolder holder, int position, @NonNull Comment model) {
+        holder.txtComment.setText(model.getCommentText());
+        holder.txtUserName.setText(model.getUserName());
+    }
+
+    @NonNull
+    @Override
+    public CommentHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_comment, parent, false);
+        return new CommentHolder(view);
+    }
+
+    public void deleteComment(int position) {
+        getSnapshots().getSnapshot(position).getReference().delete();
+    }
+
+
+    class CommentHolder extends RecyclerView.ViewHolder{
+        TextView txtUserName,txtComment;
+        public CommentHolder(@NonNull View itemView) {
+            super(itemView);
+            txtUserName = itemView.findViewById(R.id.txtUserName);
+            txtComment = itemView.findViewById(R.id.txtComment);
+
+        }
+    }
 }
