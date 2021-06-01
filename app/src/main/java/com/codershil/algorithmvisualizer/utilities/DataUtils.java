1fe32500dacd51f2164c12090774fc91fbf0e9fa
shil-alone <shilalone007@gmail.com>
2021-06-01 16:06:59 +0530

    minor code changes

--- app/src/main/java/com/codershil/algorithmvisualizer/utilities/DataUtils.java
+++ app/src/main/java/com/codershil/algorithmvisualizer/utilities/DataUtils.java
@@ -6,33 +6,31 @@ import java.util.Random;
 public class DataUtils {
 
 
-    public static int[] generateRandomArray(int size){
+    public static int[] generateRandomArray(int size) {
         int[] randomArray = new int[size];
         Random rnd = new Random();
 
-        for (int i=0; i<size; i++){
+        for (int i = 0; i < size; i++) {
             int randomNumber = rnd.nextInt(size);
-            if (randomNumber == 0){
+            if (randomNumber == 0) {
                 randomArray[i] = 1;
-            }
-            else if (randomNumber == size-1){
-                if (randomNumber/2 > 0 ) {
-                    randomArray[i] = randomNumber - rnd.nextInt(randomNumber / 2);
+            } else if (randomNumber == size - 1) {
+                if (randomNumber / 2 > 0) {
+                    int newRandom = randomNumber - rnd.nextInt(randomNumber / 2);
+                    if (newRandom < 0) {
+                        randomArray[i] = randomNumber;
+                    } else {
+                        randomArray[i] = newRandom;
+                    }
+                } else {
+                    randomArray[i] = randomNumber;
                 }
-                else {
-                    randomArray[i] = randomNumber ;
-                }
-            }
-            else {
+            } else {
                 randomArray[i] = randomNumber + 1;
             }
         }
         return randomArray;
     }
-
-
-
-    
 
 
 }
