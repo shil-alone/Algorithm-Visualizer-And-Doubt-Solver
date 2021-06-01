2872d17f39e12101ba04a4c240dcb276456a2aff
shil-alone <shilalone007@gmail.com>
2021-06-01 16:01:07 +0530

    handled like click on post

--- app/src/main/java/com/codershil/algorithmvisualizer/adapters/PostAdapter.java
+++ app/src/main/java/com/codershil/algorithmvisualizer/adapters/PostAdapter.java
@@ -1,9 +1,9 @@
 package com.codershil.algorithmvisualizer.adapters;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,9 +13,8 @@ import com.codershil.algorithmvisualizer.R;
 import com.codershil.algorithmvisualizer.models.Post;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
-
-import java.util.ArrayList;
 
 public class PostAdapter extends FirestoreRecyclerAdapter<Post, PostAdapter.PostHolder> {
     private OnItemClickListener listener;
@@ -28,6 +27,15 @@ public class PostAdapter extends FirestoreRecyclerAdapter<Post, PostAdapter.Post
     protected void onBindViewHolder(@NonNull PostHolder postHolder, int i, @NonNull Post post) {
         postHolder.txtName.setText(post.getUserName());
         postHolder.txtPostContent.setText(post.getPostContent());
+        postHolder.txtLikeCount.setText(String.valueOf(post.getLikedBy().size()));
+        postHolder.txtCommentCount.setText(String.valueOf(post.getCommentedBy().size()));
+
+        if (post.getLikedBy().contains(FirebaseAuth.getInstance().getCurrentUser().getUid())) {
+            postHolder.btnLike.setImageResource(R.drawable.ic_liked);
+        } else {
+            postHolder.btnLike.setImageResource(R.drawable.ic_baseline_thumb_up_alt_24);
+        }
+
     }
 
     @NonNull
@@ -43,7 +51,9 @@ public class PostAdapter extends FirestoreRecyclerAdapter<Post, PostAdapter.Post
     }
 
     public interface OnItemClickListener {
-        void onItemClick(DocumentSnapshot documentSnapshot, int position);
+        void onLikeClick(DocumentSnapshot documentSnapshot, int position);
+
+        void onCommentClick(DocumentSnapshot documentSnapshot, int position);
     }
 
     public void onItemClickListener(OnItemClickListener listener) {
@@ -53,26 +63,43 @@ public class PostAdapter extends FirestoreRecyclerAdapter<Post, PostAdapter.Post
 
     class PostHolder extends RecyclerView.ViewHolder {
 
-        TextView txtName, txtPostContent;
+        TextView txtName, txtPostContent, txtLikeCount, txtCommentCount;
+        ImageView btnLike, btnComment;
+
 
         public PostHolder(@NonNull View itemView) {
             super(itemView);
-            txtName = itemView.findViewById(R.id.txtName);
-            txtPostContent = itemView.findViewById(R.id.txtPostContent);
+            txtName = itemView.findViewById(R.id.txtUserName);
+            txtPostContent = itemView.findViewById(R.id.txtComment);
+            btnLike = itemView.findViewById(R.id.imgLike);
+            btnComment = itemView.findViewById(R.id.imgComments);
+            txtLikeCount = itemView.findViewById(R.id.txtLikeCount);
+            txtCommentCount = itemView.findViewById(R.id.txtCommentCount);
 
-            itemView.setOnClickListener(new View.OnClickListener() {
+
+            btnLike.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     int position = getAdapterPosition();
-                    if (position != RecyclerView.NO_POSITION && listener!=null ) {
-                        listener.onItemClick(getSnapshots().getSnapshot(position),position);
+                    if (position != RecyclerView.NO_POSITION && listener != null) {
+                        listener.onLikeClick(getSnapshots().getSnapshot(position), position);
                     }
                 }
             });
+
+            btnComment.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION && listener != null) {
+                        listener.onCommentClick(getSnapshots().getSnapshot(position), position);
+                    }
+                }
+            });
+
         }
 
     }
-
 
 
 }
