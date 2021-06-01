4b1549fc355905d5d181a0e6e4ff0617b7f26cbe
shil-alone <shilalone007@gmail.com>
2021-06-01 16:04:58 +0530

    created model class for comment

--- app/src/main/java/com/codershil/algorithmvisualizer/models/Comment.java
+++ app/src/main/java/com/codershil/algorithmvisualizer/models/Comment.java
@@ -1,4 +1,71 @@
 package com.codershil.algorithmvisualizer.models;
 
+import com.google.firebase.firestore.ServerTimestamp;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Comment {
+    String userName, commentText, userUid, postId,documentId;
+
+
+    @ServerTimestamp
+    private Date createdAt;
+
+    public Comment() {
+    }
+
+    public Comment(String userName, String commentText, String userUid, String postId) {
+        this.userName = userName;
+        this.commentText = commentText;
+        this.userUid = userUid;
+        this.postId = postId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getCommentText() {
+        return commentText;
+    }
+
+    public void setCommentText(String commentText) {
+        this.commentText = commentText;
+    }
+
+    public String getUserUid() {
+        return userUid;
+    }
+
+    public void setUserUid(String userUid) {
+        this.userUid = userUid;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(String documentId) {
+        this.documentId = documentId;
+    }
 }
