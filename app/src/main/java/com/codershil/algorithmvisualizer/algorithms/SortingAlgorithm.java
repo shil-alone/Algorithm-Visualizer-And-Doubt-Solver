91e56e54f3aa236a93cc67ee67a008c2a94ecd36
shil-alone <shilalone007@gmail.com>
2021-06-01 16:03:44 +0530

    created an generic class for sorting algorithms

--- app/src/main/java/com/codershil/algorithmvisualizer/algorithms/SortingAlgorithm.java
+++ app/src/main/java/com/codershil/algorithmvisualizer/algorithms/SortingAlgorithm.java
@@ -1,4 +1,43 @@
 package com.codershil.algorithmvisualizer.algorithms;
 
+import android.app.Activity;
+
+import com.codershil.algorithmvisualizer.visualizer.SortingVisualizer;
+
 public class SortingAlgorithm {
+    SortingVisualizer visualizer;
+    Activity activity;
+    boolean isSorting = false;
+    int[] randomArray;
+    int time = 100;
+
+    public void colSwap(int col1,int col2){
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                visualizer.colSwap(col1,col2);
+            }
+        });
+    }
+    public void colIndex(int high1,int high2){
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                visualizer.colIndex(high1,high2);
+            }
+        });
+    }
+
+    public void setTime(int time){
+        this.time = time;
+    }
+
+    public int getTime(){
+        return time;
+    }
+
+    public void setSorting(boolean sorting) {
+        isSorting = sorting;
+    }
+
 }
