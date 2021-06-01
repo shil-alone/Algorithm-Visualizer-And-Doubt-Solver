a365633691b3b89d40b7f2e7f2ed3c9f97a68d1f
shil-alone <shilalone007@gmail.com>
2021-06-01 16:03:11 +0530

    changed parent class and ran bubble sort in new thread

--- app/src/main/java/com/codershil/algorithmvisualizer/algorithms/BubbleSort.java
+++ app/src/main/java/com/codershil/algorithmvisualizer/algorithms/BubbleSort.java
@@ -4,82 +4,33 @@ import android.app.Activity;
 import android.os.Handler;
 import android.os.Looper;
 
+import com.codershil.algorithmvisualizer.activities.SortingActivity;
 import com.codershil.algorithmvisualizer.utilities.DataUtils;
 import com.codershil.algorithmvisualizer.visualizer.SortingVisualizer;
 
 
-
-public class BubbleSort {
-    SortingVisualizer visualizer;
-    Activity activity;
-    Thread thread;
-    ExampleRunnable runnable;
-    boolean isSorting = false;
-    int[] randomArray;
-    int i ,j;
-    int high1 = -1 ;
+public class BubbleSort extends SortingAlgorithm {
+    int i, j;
+    int high1 = -1;
     int high2 = -1;
-    int time = 100;
 
-    public BubbleSort(SortingVisualizer visualizer,int sizeOfArray, Activity activity){
+    public BubbleSort(SortingVisualizer visualizer, int sizeOfArray, Activity activity) {
         this.visualizer = visualizer;
         this.activity = activity;
         randomArray = DataUtils.generateRandomArray(sizeOfArray);
         visualizer.setRandomArray(randomArray);
     }
     
-    public void colSwap(int col1,int col2){
-        activity.runOnUiThread(new Runnable() {
+    public void sort() {
+        i = 0;
+        j = 0;
+        new Thread(new Runnable() {
             @Override
             public void run() {
-                visualizer.colSwap(col1,col2);
-            }
-        });
-    }
-    public void colComp(int col1,int col2){
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                visualizer.colComp(col1,col2);
-            }
-        });
-    }
-
-
-    
-    public void sort(){
-        i = 0 ; j = 0;
-        runnable = new ExampleRunnable(time);
-        thread = new Thread(runnable);
-        thread.start();
-    }
-
-    public void setTime(int time){
-        this.time = time;
-    }
-
-    public int getTime(){
-        return time;
-    }
-
-    public void setSorting(boolean sorting) {
-        isSorting = sorting;
-    }
-
-    class ExampleRunnable implements Runnable{
-        int timeInMillis;
-        Handler mainHandler;
-        public ExampleRunnable(int timeInMillis){
-            this.timeInMillis = timeInMillis;
-            mainHandler = new Handler(Looper.getMainLooper());
-        }
-        @Override
-        public void run() {
-
                 for (i = 0; i < randomArray.length; i++) {
                     for (j = 0; j < randomArray.length - 1; j++) {
                         if (!isSorting) {
-                            timeInMillis = getTime();
+                            time = getTime();
                             if (randomArray[j] > randomArray[j + 1]) {
                                 high1 = j;
                                 high2 = j + 1;
@@ -88,19 +39,17 @@ public class BubbleSort {
                                 randomArray[j] = randomArray[j + 1];
                                 randomArray[j + 1] = temp;
                                 try {
-                                    Thread.sleep(timeInMillis);
+                                    Thread.sleep(time);
                                 } catch (InterruptedException e) {
                                     e.printStackTrace();
                                 }
-
                             }
                         }
                     }
                 }
                 colSwap(-1, -1);
-
-        }
+            }
+        }).start();
     }
 
-    
 }
