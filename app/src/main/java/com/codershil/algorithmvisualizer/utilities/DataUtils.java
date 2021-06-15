6cfa71766eb1173661b39565e3018cc8bb15b5e4
shil-alone <shilalone007@gmail.com>
2021-06-15 18:16:39 +0530

    added method to create sorted array

--- app/src/main/java/com/codershil/algorithmvisualizer/utilities/DataUtils.java
+++ app/src/main/java/com/codershil/algorithmvisualizer/utilities/DataUtils.java
@@ -32,5 +32,19 @@ public class DataUtils {
         return randomArray;
     }
 
+    public static int[] generateSortedArray(int size){
+        int[] array = generateRandomArray(size);
+        for (int i = 0 ;i<array.length;i++){
+            for (int j = 0;j<array.length-i-1;j++){
+                if (array[j]>array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+        return array;
+
+    }
 
 }
