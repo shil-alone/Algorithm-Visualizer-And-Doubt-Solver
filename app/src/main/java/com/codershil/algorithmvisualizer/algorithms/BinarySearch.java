46e67aa1cec8e4d919f987eac4e68164524b3967
shil-alone <shilalone007@gmail.com>
2021-07-12 20:06:47 +0530

    fixed element not found issue

--- app/src/main/java/com/codershil/algorithmvisualizer/algorithms/BinarySearch.java
+++ app/src/main/java/com/codershil/algorithmvisualizer/algorithms/BinarySearch.java
@@ -6,11 +6,8 @@ import android.widget.Toast;
 import com.codershil.algorithmvisualizer.utilities.DataUtils;
 import com.codershil.algorithmvisualizer.visualizer.SortingVisualizer;
 
-import java.util.Collections;
-
 public class BinarySearch extends SortingAlgorithm {
     int searchElement = 5;
-    boolean isPresent;
 
     public BinarySearch(SortingVisualizer visualizer, int sizeOfArray, Activity activity) {
         this.visualizer = visualizer;
@@ -25,16 +22,13 @@ public class BinarySearch extends SortingAlgorithm {
             public void run() {
                 int low = 0;
                 int high = randomArray.length;
-                delay(time * 2);
-
-                while (low < high) {
+                while (low <= high) {
                     int mid = (low + high) / 2;
                     colComp(mid, -1);
-                    delay(time*2);
+                    delay(time * 2);
                     // checking at mid position
                     if (randomArray[mid] == randomArray[searchElement]) {
-                        isPresent = true;
-                        colSwap(mid,-1);
+                        colSwap(mid, -1);
                         activity.runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
@@ -45,7 +39,7 @@ public class BinarySearch extends SortingAlgorithm {
                         break;
                     }
 
-                    // if element to search is greater than mid then search in right sub array
+                    // if element to search is greater than mid then search in right subArray else search in left subArray
                     if (randomArray[mid] < randomArray[searchElement]) {
                         low = mid + 1;
                     } else {
@@ -54,14 +48,6 @@ public class BinarySearch extends SortingAlgorithm {
                     colComp(low, high);
                     delay(time * 2);
 
-                }
-                if (!isPresent) {
-                    activity.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(activity, "element not present in array", Toast.LENGTH_SHORT).show();
-                        }
-                    });
                 }
             }
         }).start();
