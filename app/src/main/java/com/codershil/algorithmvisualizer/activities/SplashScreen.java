82ee823a0c934b6b1c4c1a9e17a2cfdcf29d9441
shil-alone <shilalone007@gmail.com>
2021-07-12 20:04:56 +0530

    added splash screen

--- app/src/main/java/com/codershil/algorithmvisualizer/activities/SplashScreen.java
+++ app/src/main/java/com/codershil/algorithmvisualizer/activities/SplashScreen.java
@@ -1,11 +1,18 @@
-package com.codershil.jokesapp;
+package com.codershil.algorithmvisualizer.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
+
+import com.codershil.algorithmvisualizer.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SplashScreen extends Activity {
     private static boolean splashLoaded = false;
@@ -13,23 +20,43 @@ public class SplashScreen extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.status_bar_login));
+        }
+
         if (!splashLoaded){
             setContentView(R.layout.splash);
             int secondDelay = 1;
             new Handler().postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                    if (FirebaseAuth.getInstance().getCurrentUser()!=null){
+                        startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                    }
+                    else{
+                        startActivity(new Intent(SplashScreen.this,LoginActivity.class));
+                    }
                     finish();
                 }
-            },secondDelay*500);
+            },secondDelay*1000);
             splashLoaded = true;
         }
         else{
-            Intent gotoMainActivity = new Intent(SplashScreen.this,MainActivity.class);
-            gotoMainActivity.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-            startActivity(gotoMainActivity);
+            if (FirebaseAuth.getInstance().getCurrentUser()!=null){
+                Intent gotoMainActivity = new Intent(SplashScreen.this,MainActivity.class);
+                gotoMainActivity.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(gotoMainActivity);
+            }
+            else{
+                Intent gotToLoginActivity = new Intent(SplashScreen.this, LoginActivity.class);
+                gotToLoginActivity.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(gotToLoginActivity);
+            }
             finish();
+
         }
 
     }
